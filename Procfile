6464f58d159d44a9fe26b6b9afd26da164c048af
Cr0wHogan <58595537+Cr0wHogan@users.noreply.github.com>
2020-10-29 15:30:57 -0300

    1.1

--- Procfile
+++ Procfile
@@ -1,9 +1,1 @@
 web: gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
-"scripts": {
-"dev": "react-scripts start",
-"start": "serve -s build",
-"build": "react-scripts build",
-"test": "react-scripts test --env=jsdom",
-"eject": "react-scripts eject",
-"heroku-postbuild": "npm run build"
-}
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # main.py
 from fastapi import FastAPI
 app = FastAPI()
-
+"scripts": {
+"dev": "react-scripts start",
+"start": "serve -s build",
+"build": "react-scripts build",
+"test": "react-scripts test --env=jsdom",
+"eject": "react-scripts eject",
+"heroku-postbuild": "npm run build"
+}
 @app.get("/")
 def hello():
     return {"message":"Hello TutLinks.com"}
