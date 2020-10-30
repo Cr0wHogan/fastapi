152d4f2d1daf7d1347474de4412bce11ddb26d69
Cr0wHogan <58595537+Cr0wHogan@users.noreply.github.com>
2020-10-30 14:09:57 -0300

    1.4

--- crud.py
+++ crud.py
@@ -1,4 +1,4 @@
-from . import models, schemas
+import models, schemas
 from sqlalchemy.orm import Session
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
--- schemas.py
+++ schemas.py
@@ -1,7 +1,6 @@
 from typing import List, Optional
 
 from pydantic import BaseModel
-
 class ItemBase(BaseModel):
     title: str
     description: Optional[str] = None
