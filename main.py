dd1916107cd0a4d065729cf6715684cfcfbf8239
Cr0wHogan <58595537+Cr0wHogan@users.noreply.github.com>
2020-10-31 12:22:48 -0300

    getproject by email

--- main.py
+++ main.py
@@ -61,3 +61,10 @@ def get_project(project_id: int, db: Session = Depends(get_db)):
     if db_project is None:
         raise HTTPException(status_code=404, detail="Proyecto no encontrado")
     return db_project
+
+@app.get("/users/{email}", response_model=schemas.UserBase)
+def get_projects_by_email(email: str, db: Session = Depends(get_db)):
+    db_project = crud.get_user_by_email(db, email=email)
+    if db_project is None:
+        raise HTTPException(status_code=404, detail="El usuario no participa de ningún proyecto")
+    return db_project
