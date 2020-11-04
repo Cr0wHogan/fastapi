28f1032471a6054a472190309df24352d7c7e667
Cr0wHogan <58595537+Cr0wHogan@users.noreply.github.com>
2020-11-04 16:20:54 -0300

    prueba10

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 import crud, models, schemas
-from database import SessionLocal, engine
+from database import sessionmaker, engine
 
 Session = sessionmaker(engine)  
 db = Session()
